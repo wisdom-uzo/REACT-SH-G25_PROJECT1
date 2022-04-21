89e91a05a8e9d477563e474de71ee2f49f2f4a18
unknown <wisdomuzodimna@gmail.com>
2022-04-21 16:15:11 +0100

    i just made changes to the footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import './Footer.css'
+import { BsFacebook, BsGithub, BsLinkedin, BsTwitter, BsInstagram } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='app_footer_container'>
+        <div className="app_footer-sec1">
+
+            <div className="">
+                <h3>OPENING HOURS</h3>
+
+                <p>Monday till Thurday: <span>09:00AM-10:30Pm</span></p>
+                <p>Friday till Sunday:<span> 09:00Am-11:00pm</span></p>
+            </div>
+
+            <div className="">
+                <h3>CONTACT US</h3>
+
+                <p>Email: <span>SH_REACT_G25@gmail.com</span></p>
+                 <p>Tel: <span>+234 4318 2441</span></p>
+            </div>
+
+            <div className="">
+              <h3>CONTACT US</h3>
+
+              <p>Email: <span>SH_REACT_G25@gmail.com</span></p>
+              <p>Tel: <span>+234 4318 2441</span></p>
+            </div>
+
+            <div className="">
+            <h3>SOCIAL </h3>
+
+              <ul className='footer_social'>
+                <li><a href=""><BsFacebook className='footer_icon'/>Facebook</a></li>
+                <li><a href=""><BsGithub className='footer_icon' />GitHub</a></li>
+                <li><a href=""><BsTwitter className='footer_icon' />Twitter</a></li>
+                <li><a href=""><BsLinkedin className='footer_icon' />Linkedin</a></li>
+                <li><a href=""><BsInstagram className='footer_icon' />Instagram</a></li>
+              </ul>   
+          </div>
+
+
+        </div>
+    </div>
   )
 }
 
